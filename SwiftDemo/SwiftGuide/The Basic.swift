50358271d7503fe3d5072fa51672ee757526261e
流年释残忆 <291753906@qq.com>
2018-04-17 22:23:41 +0800

    add The Basic

--- SwiftDemo/SwiftGuide/The Basic.swift
+++ SwiftDemo/SwiftGuide/The Basic.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+typealias AudioSample = UInt16//类型别名
+
 class The_Basic: UIViewController {
 
     var taArray = ["The Basic","Basic Operations"]
@@ -23,6 +25,12 @@ class The_Basic: UIViewController {
         IntegerBounds()
         typeInference()
         numericLiterals()
+        integerConversion()
+        integerAndFloatConversion()
+        typeAliases()
+        Boolean()
+        tuples()
+        optionals()
     }
     
     /// 定义常量/变量
@@ -95,5 +103,89 @@ class The_Basic: UIViewController {
         let hexadecimalInteger = 0x11     // 17 in hexadecimal notation
         print(decimalInteger,binaryInteger,octalInteger,hexadecimalInteger)
     }
+    
+    
+    /// 整数转换
+    func integerConversion() {
+//        let cannotBeNegative: UInt8 = -1 //无符号数不能表示负数
+//        let tooBig: Int8 = Int8.max + 1  //Int8 cannot store a number larger than its maximum value,
+        
+        let twoThousand: UInt16 = 2_000
+        let one: UInt8 = 1
+        let twoThousandAndOne = twoThousand + UInt16(one) //类型不同 不能相加 需要转换
+        print(twoThousandAndOne)
+    }
+    
+    
+    /// 整数和浮点型
+    func integerAndFloatConversion()  {
+        let three = 3
+        let pointOneFourOneFiveNine = 0.14159
+        
+        let pi = Double(three) + pointOneFourOneFiveNine//类型推断为Double
+        
+        print(Double(three),pi)
+    }
+    
+    /// 类型别名
+    func typeAliases() {
+        let maxAmplitudeFound = AudioSample.min//AudioSample是UInt16的别名
+        print(maxAmplitudeFound)
+    }
+    
+    
+    /// 布尔类型
+    func Boolean() {
+        let turnipsAreDelicious = false
+        if turnipsAreDelicious {
+            print("Mmm, tasty turnips!")
+        } else {
+            print("Eww, turnips are horrible.")
+        }
+        
+        //Swiftz阻止非Boolean值代表Boolean
+        let i = 1
+//        if i {// 'Int' is not convertible to 'Bool'
+//            print("")
+//        }
+        if i == 1 {//这样才是可以的
+            
+        }
+    }
+    
+    
+    /// 元组
+    func tuples() {
+        let http404Error = (404, "NotFound")// http404Error is of type (Int, String), and equals (404, "Not Found")
+        print(http404Error)
+        
+        let (statusCode, statusMessage) = (403, "Forbidion")
+        print("the statusCode is \(statusCode)")
+        print("the statusCode is \(statusMessage)")
+        
+        let (code, _) = (500, "Error")//如果只需要其中的某个值，其他值可以用_代替
+        print("the statusCode is \(code)")
+        
+        //可以在定义的时候赋初值
+        let http200Status = (statusCode: 200, description: "OK")
+        print("The status code is \(http200Status.statusCode)")
+        print("The status description is \(http200Status.description)")
+        
+    }
+    
+    
+    /// 可选值
+    func optionals() {
+        let possibleNumber = "12"
+        let conversionNumber = Int(possibleNumber)
+        print(conversionNumber as Any)
+        
+        var serverResponseCode: Int? = 404
+        serverResponseCode = nil
+        
+        let sureAnswer: String?//可选值自动初始化为nil
+    }
+    
+    
     
 }
