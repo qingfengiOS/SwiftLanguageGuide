e79b321cc2ba0c3af02ef29467cabfe2cffe2627
流年释残忆 <291753906@qq.com>
2018-05-21 21:28:28 +0800

    compelete 'OptionChaining'

--- SwiftDemo/SwiftGuide/OptionalChaining.swift
+++ SwiftDemo/SwiftGuide/OptionalChaining.swift
@@ -45,6 +45,11 @@ class OptionalChaining: UIViewController {
         
         
         accessingPropertiesThroughOptionalChaining()
+        callingMethodThroughOptionChaining()
+        accessingSubscriptsThroughOptionalChaining()
+        accessingSubscriptsOfOptionalType()
+        linkingMultipleLevelOfChaining()
+        chainingOnMethodsWithOptionalReturnValues()
     }
     
     //MARK:--通过可选链式调用访问属性
@@ -82,7 +87,7 @@ class OptionalChaining: UIViewController {
     
     
     //MARK:--通过可选链式调用调用方法
-    func callingMethodThroughOptionChaining () {
+    func callingMethodThroughOptionChaining() {
         let john = Personer2()
         if john.residence?.printNumberOfRooms() != nil {
             print("It was possible to print the number of rooms.")
@@ -98,9 +103,107 @@ class OptionalChaining: UIViewController {
         } else {
             print("It was not possible to set the address.")
         }
-
-        //MARK:--Accessing Subscripts Through Optional Chaining
+    }
+    
+    //MARK:--使用可选链式调用访问下标
+    func accessingSubscriptsThroughOptionalChaining() {
+        let john = Personer2()
         
+        if let firstRoomName = john.residence?[0].name {
+            print("The first room name is \(firstRoomName).")
+        } else {
+            print("Unable to retrieve the first room name")
+        }
+        
+        john.residence?[0] = Room(name: "Bathroom")
+        
+        let johnHouse = Residence2()
+        johnHouse.rooms.append(Room(name: "Living Room"))
+        johnHouse.rooms.append(Room(name: "Kitchen"))
+        
+        if let firstRoom = john.residence?[0].name {
+            print("The forst room name is \(firstRoom).")
+        } else {
+            print("Unable to retrieve the first room name.")
+        }
+        
+    }
+    
+    //MARK:--访问可选类型的下标
+    /*
+     如果下标返回可选类型值，比如 Swift 中 类型的键的下标，可以在下标的结尾括号后面放一个问号 来在其可选返回值上进行可选链式调用:
+     */
+    func accessingSubscriptsOfOptionalType() {
+        var testScores = ["Dave": [86,82,84], "Bev": [79,89,90]]
+        testScores["Dave"]?[0] = 99
+        testScores["Bev"]?[0] += 1
+        testScores["Briam"]?[0] = 72
+        for (name, score) in testScores {
+            print("name is \(name),score = \(score)")
+        }
+    }
+    
+    //MARK:--连接多层可选链式调用
+    /*
+     可以通过连接多个可选链式调用在更深的模型层级中访问属性、方法以及下标。然而，多层可选链式调用不会增加返回值的可选层级。
+     
+     也就是说:
+     • 如果你访问的值不是可选的，可选链式调用将会返回可选值。
+     • 如果你访问的值就是可选的，可选链式调用不会让可选返回值变得“更可选”。
+     
+     因此:
+     • 通过可选链式调用访问一个 Int 值，将会返回 Int? ，无论使用了多少层可选链式调用。
+     • 类似的，通过可选链式调用访问 Int? 值，依旧会返回 Int? 值，并不会返回 Int?? 。
+     */
+    func linkingMultipleLevelOfChaining() {
+        let john = Personer2()
+        if let johnsStreet = john.residence?.address?.street {
+            print("John's street name is \(johnsStreet).")
+        } else {
+            print("Unable to retrieve the address.")
+        }
+        
+        
+        let johnsAddress = Address()
+        johnsAddress.buildingName = "The Larches"
+        johnsAddress.buildingNumber = "99"
+        johnsAddress.street = "Laurel Street"
+        john.residence?.address = johnsAddress
+        
+        if let johnsStreet = john.residence?.address?.street {
+            print("John's street name is \(johnsStreet).")
+        } else {
+            print("Unable to retrieve the address.")
+        }
+    }
+    
+    //MARK:--在方法的可选返回值上进行可选链式调用
+    func chainingOnMethodsWithOptionalReturnValues() {
+        //下面的例子中，通过可选链式调用来调用 Address 的 buildingIdentifier() 方法。这个方法返回 String? 类型 的值。如上所述，通过可选链式调用来调用该方法，最终的返回值依旧会是 String? 类型:
+        
+        let john = Personer2()
+        
+        john.residence = Residence2()
+        
+        let johnsAddress = Address()
+        johnsAddress.buildingName = "The Larches"
+        johnsAddress.buildingNumber = "99"
+        johnsAddress.street = "Laurel Street"
+        john.residence?.address = johnsAddress
+        
+        if let buildingIdentifier = john.residence?.address?.buildingIdentifier() {
+            print("John's building identifier is \(buildingIdentifier).")
+        }
+        
+        if let beginsWithThe =
+            john.residence?.address?.buildingIdentifier()?.hasPrefix("The") {
+            if beginsWithThe {
+                print("John's building identifier begins with \"The\".")
+            } else {
+                print("John's building identifier does not begin with \"The\".")
+            }
+            //在上面的例子中，在方法的圆括号后面加上问号是因为你要在 buildingIdentifier() 方法的可选返回值上进行 可选链式调用，而不是方法本身。
+        }
     }
     
 }
@@ -150,7 +253,7 @@ class Residence2 {
 }
 
 class Room {
-    let name: String
+    var name: String
     init(name: String) {
         self.name = name
     }
