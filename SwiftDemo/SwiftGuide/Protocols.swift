b3f63c9eba75c83778a9645e8d58fd61a5ddd448
流年释残忆 <291753906@qq.com>
2018-05-27 22:25:14 +0800

    update Protocols

--- SwiftDemo/SwiftGuide/Protocols.swift
+++ SwiftDemo/SwiftGuide/Protocols.swift
@@ -127,6 +127,163 @@ enum OnOffSwitch: Togglable {
     }
 }
 
+//MARK:-构造器要求
+/*
+ 协议可以要求遵循协议的类型实现指定的构造器。你可以像编写普通构造器那样，在协议的定义里写下构造器的声明，但不需要写花括号和构造器的实体
+ */
+protocol InitializeProtocol {
+    init(someParamter: Int)
+}
+
+/*
+ 构造器要求在类中的实现
+ 
+ 你可以在遵循协议的类中实现构造器，无论是作为指定构造器，还是作为便利构造器。无论哪种情况，你都必须 为构造器实现标上 required 修饰符:
+*/
+class InitializeProtocolClass: InitializeProtocol {
+    required init(someParamter: Int) {
+        // 这里是构造器的实现部分
+        //使用 required 修饰符可以确保所有子类也必须提供此构造器实现，从而也能符合协议。
+    }
+}
+//如果一个子类重写了父类的指定构造器，并且该构造器满足了某个协议的要求，那么该构造器的实现需要同时标 注 required 和 override 修饰符:
+protocol SomesProtocol {
+    init()
+}
+class SomesSuperClass {
+    init() {
+        // 这里是构造器的实现部分
+    }
+}
+class SomesSubClass: SomesSuperClass, SomesProtocol {
+    // 因为遵循协议，需要加上 required
+    // 因为继承自父类，需要加上 override
+    required override init() {
+        // 这里是构造器的实现部分
+    }
+}
+//可失败构造器要求
+//遵循协议的类型可以通过可失败构造器( init? )或非可失败构造器( init )来满足协议中定义的可失败构造器 要求。协议中定义的非可失败构造器要求可以通过非可失败构造器( init )或隐式解包可失败构造器( init! )来满足
+
+//MARK:-协议作为类型
+/*
+ 尽管协议本身并未实现任何功能，但是协议可以被当做一个成熟的类型来使用。
+ 
+ 协议可以像其他普通类型一样使用，使用场景如下:
+ 
+ • 作为函数、方法或构造器中的参数类型或返回值类型
+ • 作为常量、变量或属性的类型
+ • 作为数组、字典或其他容器中的元素类型
+ 
+ 协议是一种类型，因此协议类型的名称应与其他类型(例如 Int ， Double ， String )的写法相同，使用大写 字母开头的驼峰式写法，例如(FullyNamed 和RandomNumberGenerator)。
+ */
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(side: Int, generator: RandomNumberGenerator) {
+        self.sides = side
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+//MARK:-委托(代理)模式
+/*
+ 委托是一种设计模式，它允许类或结构体将一些需要它们负责的功能委托给其他类型的实例。委托模式的实现很简单:定义协议来封装那些需要被委托的功能，这样就能确保遵循协议的类型能提供这些功能。委托模式可以用来响应特定的动作，或者接收外部数据源提供的数据，而无需关心外部数据源的类型。
+ 下面的例子定义了两个基于骰子游戏的协议:
+ */
+protocol DiceGame {
+    var dice: Dice { get }
+    func play()
+}
+
+protocol DiceGameDelegate {
+    func gameDidStart(_ game: DiceGame)
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int)
+    func gameDidEnd(_ game: DiceGame)
+    /*
+     DiceGame 协议可以被任意涉及骰子的游戏遵循。DiceGameDelegate 协议可以被任意类型遵循，用来追踪 Game 的游戏过程。
+     */
+}
+
+class SnakesAndLadders: DiceGame {
+    let finalSquare = 25
+    let dice = Dice(side: 6, generator: LinearCongruentialGenerator())
+    var square = 0
+    var board: [Int]
+    init() {
+        board = [Int](repeating: 0, count: finalSquare + 1)
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    }
+    
+    var delegate: DiceGameDelegate?
+    func play() {
+        square = 0
+        delegate?.gameDidStart(self)
+        gameLoop: while square != finalSquare {
+            let diceRoll = dice.roll()
+            delegate?.game(self, didStartNewTurnWithDiceRoll: diceRoll)
+            switch square + diceRoll {
+            case finalSquare:
+                break gameLoop
+            case let newSquare where newSquare > finalSquare:
+                break gameLoop
+            default:
+                square += diceRoll
+                square += board[square]
+            }
+        }
+        delegate?.gameDidEnd(self)
+    }
+    /*
+     This version of the game is wrapped up as a class called SnakesAndLadders, which adopts the DiceGame protocol. It provides a gettable dice property and a play() method in order to conform to the protocol. (The dice property is declared as a constant property because it doesn’t need to change after initialization, and the protocol only requires that it must be gettable.)
+     
+     The Snakes and Ladders game board setup takes place within the class’s init() initializer. All game logic is moved into the protocol’s play method, which uses the protocol’s required dice property to provide its dice roll values.
+     
+     Note that the delegate property is defined as an optional DiceGameDelegate, because a delegate isn’t required in order to play the game. Because it’s of an optional type, the delegate property is automatically set to an initial value of nil. Thereafter, the game instantiator has the option to set the property to a suitable delegate. Because the DiceGameDelegate protocol is class-only, you can declare the delegate to be weak to prevent reference cycles.
+     
+     DiceGameDelegate provides three methods for tracking the progress of a game. These three methods have been incorporated into the game logic within the play() method above, and are called when a new game starts, a new turn begins, or the game ends.
+     
+     Because the delegate property is an optional DiceGameDelegate, the play() method uses optional chaining each time it calls a method on the delegate. If the delegate property is nil, these delegate calls fail gracefully and without error. If the delegate property is non-nil, the delegate methods are called, and are passed the SnakesAndLadders instance as a parameter.
+     */
+}
+
+class DiceGameTracker: DiceGameDelegate {
+    var numberOfTurns = 0
+    func gameDidStart(_ game: DiceGame) {
+        numberOfTurns = 0
+        if game is SnakesAndLadders {
+            print("Started a new game of Snakes and Ladders")
+        }
+        print("The game is using a \(game.dice.sides)-sided dice")
+    }
+    
+    func game(_ game: DiceGame, didStartNewTurnWithDiceRoll diceRoll: Int) {
+        numberOfTurns += 1
+        print("Rolled a \(diceRoll)")
+    }
+    
+    func gameDidEnd(_ game: DiceGame) {
+        print("The game lasted for \(numberOfTurns) turns")
+    }
+
+    /*
+     DiceGameTracker实现了DiceGameDelegate协议要求的三个方法用来记录游戏已经进行的轮数。当游戏开始时，numberOfTurn属性被赋值为0，然后在每一轮中递增，游戏结束之后，打印游戏的总轮数
+     
+     gameDidStart(_:)方法从game参数获取游戏信息并且打印。game参数是DiceGame类型，而不是SnakeAndLadders类型，所以在gameDidStart(_:)发方法中只能访问DiceGame协议中的内容。当然，SnakeAdnLadders的方法也可以在类型转换之后调用。在上例代码中，通过is操作符game是否为SnakeAndLadders类型的实例，如果是，则打印出相应的消息。
+     
+     无论当前进行的是何种游戏，由于game符合DiceGame协议，可以确保game含有dice属性。因此，在gameDidStart(_:)方法中可以通过传入的game参数类访问dice属性，进而打印dice的sides属性的值。
+     */
+    
+    
+}
+
+
+//MARK:-Main ViewController
 class Protocols: UIViewController {
 
     override func viewDidLoad() {
@@ -158,6 +315,16 @@ class Protocols: UIViewController {
         } else {
             print("Off")
         }
+        
+        let d6 = Dice(side: 6, generator: LinearCongruentialGenerator())
+        for _ in 1...5 {
+            print("Random dice roll is \(d6.roll())")
+        }
+        
+        let tracker = DiceGameTracker()
+        let game = SnakesAndLadders()
+        game.delegate = tracker
+        game.play()
     }
     
 }
